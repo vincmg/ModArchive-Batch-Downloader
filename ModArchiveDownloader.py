5385e160d91019deedbf39f6d60ad81d20b5f42f
VinCMG <vincentchang9999@gmail.com>
2016-09-23 00:46:14 -0700

    Refactored to use argparse for commandline arguments
    Also fixed an error with trying to print the filename before it was known.

--- ModArchiveDownloader.py
+++ ModArchiveDownloader.py
@@ -1,17 +1,19 @@
-import urllib.request, sys, os, re
+import urllib.request, sys, os, re, argparse
 
 if __name__ == '__main__':
-	if sys.argv[1] in ['help', '-h', '--help']:
-		print("Usage: python3 scriptname.py save_directory FROM TO")
-		sys.exit()
+	parser = argparse.ArgumentParser(description='Tool to download a range of modules from ModArchive, specified by their moduleid number')
+	parser.add_argument('save_directory')
+	parser.add_argument('FROM')
+	parser.add_argument('TO')
 
-	savedirectory = sys.argv[1]
+	args = parser.parse_args()
+
+	savedirectory = args.save_directory
 	find_orig_filename = re.compile("filename=(.+)")
 
 	os.chdir(savedirectory)
 
-	for i in range(int(sys.argv[2]), int(sys.argv[3]) + 1):
-		print("Downloading {}".format(filename))
+	for i in range(int(args.FROM), int(args.TO) + 1):
 		filename, headers = urllib.request.urlretrieve('https://api.modarchive.org/downloads.php?moduleid={}'.format(i), filename="moduleid_{}".format(i))
 
 		match = find_orig_filename.search(headers.as_string())
@@ -19,6 +21,7 @@ if __name__ == '__main__':
 		# there are some moduleid numbers that can't be found
 		if match is not None:
 			orig_filename = match.group(1)
+			print("Downloaded {}".format(orig_filename))
 			print("\tRenaming {} to {}".format(filename, match.group(1)))
 			os.rename(filename, orig_filename)
 		else:
